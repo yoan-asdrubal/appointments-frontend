40310925f6fcb3b1647311d850e3026391ee2b2c
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-06 12:40:01 -0500

    Test(NotificationsService):Add test to add notification and delete after 5000 ms

--- src/app/core/notifications/domain/notifications.service.spec.ts
+++ src/app/core/notifications/domain/notifications.service.spec.ts
@@ -1,5 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 import {NotificationsService} from '@app/core/notifications/domain/notifications.service';
+import {Alert, AlertType} from '@app/core/notifications/domain/notifications.model';
 
 describe('DatasourceService', () => {
     let service: NotificationsService;
@@ -15,4 +16,18 @@ describe('DatasourceService', () => {
         expect(service).toBeTruthy();
     });
 
+    it('should add Alert and delete after 5000 ms', function() {
+        jest.useFakeTimers();
+        const alert: Alert = new Alert(AlertType.SUCCESS, 'Message', 'Testing Alert success');
+        service.show(alert);
+        expect(service.alerts.length).toEqual(1);
+
+        jest.advanceTimersByTime(2000);
+
+        expect(service.alerts.length).toEqual(1);
+
+        jest.advanceTimersByTime(3000);
+        // jest.runAllTimers();
+        expect(service.alerts.length).toEqual(0);
+    });
 });
