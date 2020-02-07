b62cf059e06942267784e3100ae0e919776a2bd2
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-07 09:44:50 -0500

    Test(Navigation):Add unit test and implementation of load menu items and show them

--- src/app/layout/components/navigation/navigation.component.spec.ts
+++ src/app/layout/components/navigation/navigation.component.spec.ts
@@ -8,22 +8,60 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {NavigationComponent} from './navigation.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {DatasourceService} from '@app/core/datasource/domain/datasource.service';
+import {MenuItemService} from '@app/core/model/menu-item/menu-item.service';
+import {MenuItemQuery} from '@app/core/model/menu-item/menu-item-query.service';
+import {MenuItemStore} from '@app/core/model/menu-item/menu-item-store.service';
+import {of} from 'rxjs';
 
+const items = [
+    {
+        'id': 1,
+        'label': 'Appointments',
+        'route': '/appointment',
+        'cy': 'appointment-route'
+    },
+    {
+        'id': 2,
+        'label': 'Logs',
+        'route': '/logs',
+        'cy': 'log-route'
+    }
+];
 describe('NavigationComponent', () => {
     let component: NavigationComponent;
     let fixture: ComponentFixture<NavigationComponent>;
-
+    let datasourceService;
     beforeEach(async(() => {
+        datasourceService = {
+            request: jest.fn()
+        };
+        datasourceService.request = jest.fn(() => {
+            return of(items);
+        });
+
         TestBed.configureTestingModule({
             declarations: [NavigationComponent],
             imports: [
                 NoopAnimationsModule,
+                RouterTestingModule,
+                HttpClientTestingModule,
                 LayoutModule,
                 MatButtonModule,
                 MatIconModule,
                 MatListModule,
                 MatSidenavModule,
                 MatToolbarModule,
+            ],
+            providers: [
+                MenuItemQuery,
+                MenuItemStore,
+                MenuItemService,
+                {
+                    provide: DatasourceService, useValue: datasourceService
+                }
             ]
         }).compileComponents();
     }));
@@ -36,5 +74,13 @@ describe('NavigationComponent', () => {
 
     it('should compile', () => {
         expect(component).toBeTruthy();
+
+    });
+
+    it('should show menu items', function() {
+        expect(fixture.nativeElement.querySelectorAll('.mat-list-item').length).toEqual(2);
+        items.forEach((item) => {
+            expect(fixture.nativeElement.querySelector(`[data-cy='${item.cy}']`)).toBeTruthy();
+        });
     });
 });
--- src/app/layout/components/navigation/navigation.component.ts
+++ src/app/layout/components/navigation/navigation.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {MenuItemService} from '@app/core/model/menu-item/menu-item.service';
+import {MenuItemModel} from '@app/core/model/menu-item/menu-item.model';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+    items: Observable<MenuItemModel[]>;
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+            map(result => result.matches),
+            shareReplay()
+        );
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+    constructor(private breakpointObserver: BreakpointObserver, private menuS: MenuItemService) {
+    }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    ngOnInit() {
+        this.items = this.menuS.list();
+    }
 
 }
