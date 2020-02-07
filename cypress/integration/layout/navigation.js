83e582c6432aa206e815fcf725d296daf9ae80d7
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-06 23:58:15 -0500

    e2e(navigation):Add test to navigate to appointment route and render app-appointment-component

--- cypress/integration/layout/navigation.js
+++ cypress/integration/layout/navigation.js
@@ -1,13 +1,8 @@
 describe('Navigation component', () => {
     it('should render ToHComponent when access to root route', function () {
-        cy.visit('/');
-        cy.get('app-toh-home').should('have.length', 1);
-    });
-
-    it('should load url and component when click on Dashboard option', function () {
-        cy.visit('/');
-        cy.get('#id1').click();
-        cy.url().should('include', 'dashboard');
+        cy.visit('/pathNoFound');
+        cy.get('[data-cy="appointment-route"]').click();
+        cy.get('app-appointment-container').should('have.length', 1);
     });
 
 })
