cad7fd4e798e48c20151be101b20ef7d6474cdaa
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-06 22:40:15 -0500

    Refactor(AppRouting):Add routing to pathNoFound and default route redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NavigationComponent} from '@app/layout/components/navigation/navigation.component';
+import {Error404Component} from '@app/layout/components/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: NavigationComponent,
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'appointment'
+            },
+            {
+                path: 'pathNotFound', component: Error404Component
+            },
+            {
+                path: '**', redirectTo: 'pathNotFound'
+            }]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
