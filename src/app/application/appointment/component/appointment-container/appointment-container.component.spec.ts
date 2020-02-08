1af34b90e55fe33610c0b1f3f54efc893689d9cf
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-07 23:26:24 -0500

    Test(Appointment):Add test to get all appointment in appointment-container

--- src/app/application/appointment/component/appointment-container/appointment-container.component.spec.ts
+++ src/app/application/appointment/component/appointment-container/appointment-container.component.spec.ts
@@ -1,25 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AppointmentContainerComponent } from './appointment-container.component';
+import {AppointmentContainerComponent} from './appointment-container.component';
+import {appointmentMockData} from '@app/core/model/appointment/appointment.model';
+import {AppointmentService} from '@app/core/model/appointment/appointment.service';
+import {of} from 'rxjs';
 
 describe('AppointmentContainerComponent', () => {
-  let component: AppointmentContainerComponent;
-  let fixture: ComponentFixture<AppointmentContainerComponent>;
+    let component: AppointmentContainerComponent;
+    let fixture: ComponentFixture<AppointmentContainerComponent>;
+    let service: AppointmentService | any;
+    const data = appointmentMockData;
+    beforeEach(async(() => {
+        service = {
+            list: jest.fn(() => {
+                return of(data);
+            })
+        };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppointmentContainerComponent ]
-    })
-    .compileComponents();
-  }));
+        TestBed.configureTestingModule({
+            declarations: [AppointmentContainerComponent]
+            , providers: [
+                {provide: AppointmentService, useValue: service}
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppointmentContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppointmentContainerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get list of appointments', function() {
+        const appointments$ = component.appointments$;
+        appointments$.pipe().subscribe((items: any[]) => {
+            expect(items.length).toEqual(3);
+            expect(items).toEqual(data);
+        });
+        expect(service.list).toHaveBeenCalled();
+        expect(service.list).toHaveBeenCalledTimes(1);
+    });
 });
--- src/app/application/appointment/component/appointment-container/appointment-container.component.ts
+++ src/app/application/appointment/component/appointment-container/appointment-container.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {AppointmentService} from '@app/core/model/appointment/appointment.service';
+import {Observable} from 'rxjs';
+import {AppointmentModel} from '@app/core/model/appointment/appointment.model';
 
 @Component({
     selector: 'app-appointment-container',
@@ -6,11 +9,13 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./appointment-container.component.css']
 })
 export class AppointmentContainerComponent implements OnInit {
+    appointments$: Observable<AppointmentModel[]>;
 
-    constructor() {
+    constructor(private appointmentS: AppointmentService) {
     }
 
     ngOnInit() {
+        this.appointments$ = this.appointmentS.list();
     }
 
 }
