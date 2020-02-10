baae33526f395d44695d3282da65597d031a950e
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-10 17:43:33 -0500

    Refactor(AppointmentModel):Modify fields of interface and add class to define form model
    Add class to represent reactive form using rxweb validators.

--- src/app/core/model/appointment/appointment.model.ts
+++ src/app/core/model/appointment/appointment.model.ts
@@ -1,43 +1,79 @@
 import {ID} from '@datorama/akita';
+import {prop, required} from '@rxweb/reactive-form-validators';
 
 export interface AppointmentModel {
     id: ID;
-    from: string;
-    to: string;
+    date: string;
+    timeInit: string;
+    timeEnd: string;
     subject: string;
     description: string;
+    area: string;
+}
+
+export class AppointmentFormModel {
+    @prop()
+    id: ID;
+
+    @required()
+    date: string;
+
+    @required()
+    timeInit: string;
+
+    @required()
+    timeEnd: string;
+
+    @required()
+    subject: string;
+
+    @prop()
+    description: string;
+
+    @prop()
     area: string;
 }
 
 /**
  * A factory function that creates PersonaModel
  */
-export function createAppointment({id, from, to, subject, description, area}: Partial<AppointmentModel>) {
-    return {id: id, from: from, to: to, subject: subject, description: description, area: area} as AppointmentModel;
+export function createAppointment({id, date, timeInit, timeEnd, subject, description, area}: Partial<AppointmentModel>) {
+    return {
+        id: id,
+        date: date,
+        timeInit: timeInit,
+        timeEnd: timeEnd,
+        subject: subject,
+        description: description,
+        area: area
+    } as AppointmentModel;
 }
 
 
 export const appointmentMockData = [
         {
             id: 'id1',
-            from: new Date(2020, 1, 7, 8).toLocaleString(),
-            to: new Date(2020, 1, 7, 13).toLocaleString(),
+            date: new Date(2020, 1, 7).toLocaleString(),
+            timeInit: '10:55 AM',
+            timeEnd: '3 PM',
             subject: 'Task',
             description: 'Run task number 1',
             area: '',
         },
         {
             id: 'id2',
-            from: new Date(2020, 1, 10, 10, 30).toLocaleString(),
-            to: new Date(2020, 1, 10, 16).toLocaleString(),
+            date: new Date(2020, 1, 10).toLocaleString(),
+            timeInit: '8:55 AM',
+            timeEnd: '5 PM',
             subject: 'TODO',
             description: 'Go for todo number 1',
             area: '',
         },
         {
             id: 'id3',
-            from: new Date(2020, 1, 15, 12).toLocaleString(),
-            to: new Date(2020, 1, 15, 14).toLocaleString(),
+            date: new Date(2020, 1, 15).toLocaleString(),
+            timeInit: '12:00 PM',
+            timeEnd: '2:00 PM',
             subject: 'Launch',
             description: 'Get Launch at 12:00 PM',
             area: '',
