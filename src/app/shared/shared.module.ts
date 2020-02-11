ad1f616e4ca7fdf73b7c78a125123464d7247962
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-10 23:15:20 -0500

    Refactor(SharedModule): Add RxReactiveFormsModule to shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
+        ReactiveFormsModule,
         RxReactiveFormsModule
     ],
     exports: [
+        ReactiveFormsModule,
         RxReactiveFormsModule
     ]
 })
