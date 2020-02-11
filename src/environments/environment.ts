28cf00603647b8dcd0740f00643a491b5f879008
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-11 17:11:39 -0500

    Refactor(enviroments):Refactor appointment api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     api: {
         menu: '/assets/menu/menu.json',
-        appointment: 'https://appointments-backend.herokuapp.com'
+        appointment: 'https://appointments-backend.herokuapp.com/api'
     }
 };
 
