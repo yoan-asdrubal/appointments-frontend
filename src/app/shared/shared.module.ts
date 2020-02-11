aad06b0779ed56d37e231351f6dd489071acf835
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-10 23:50:29 -0500

    Refactor(SharedModule): Add CalendarModule to sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,24 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
 import {ReactiveFormsModule} from '@angular/forms';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        RxReactiveFormsModule
+        RxReactiveFormsModule,
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        }),
     ],
     exports: [
         ReactiveFormsModule,
-        RxReactiveFormsModule
+        RxReactiveFormsModule,
+        CalendarModule
     ]
 })
 export class SharedModule {
