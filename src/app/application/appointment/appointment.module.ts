2592753fb996f85dd848ccb89acfbe1a610be38a
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-10 23:15:41 -0500

    Refactor(SharedModule): Add SharedModule to appointmentModule

--- src/app/application/appointment/appointment.module.ts
+++ src/app/application/appointment/appointment.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AppointmentRoutingModule } from './appointment-routing.module';
-import { AppointmentContainerComponent } from './component/appointment-container/appointment-container.component';
-import { AppointmentDialogComponent } from './component/appointment-dialog/appointment-dialog.component';
+import {AppointmentRoutingModule} from './appointment-routing.module';
+import {AppointmentContainerComponent} from './component/appointment-container/appointment-container.component';
+import {AppointmentDialogComponent} from './component/appointment-dialog/appointment-dialog.component';
+import {SharedModule} from '@app/shared/shared.module';
 
 @NgModule({
-  declarations: [AppointmentContainerComponent, AppointmentDialogComponent],
-  imports: [
-    CommonModule,
-    AppointmentRoutingModule
-  ]
+    declarations: [AppointmentContainerComponent, AppointmentDialogComponent],
+    imports: [
+        CommonModule,
+        AppointmentRoutingModule,
+        SharedModule
+    ]
 })
-export class AppointmentModule { }
+export class AppointmentModule {
+}
