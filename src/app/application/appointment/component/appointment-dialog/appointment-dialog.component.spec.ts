bcef0c8c93890c4d96f35a8df4dd892ed67934d9
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-10 23:16:36 -0500

    Test(SharedModule): Add test to create appointment formGroup to specific date

--- src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.spec.ts
+++ src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.spec.ts
@@ -1,25 +1,97 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AppointmentDialogComponent } from './appointment-dialog.component';
+import {AppointmentDialogComponent} from './appointment-dialog.component';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {OverlayContainer} from '@angular/cdk/overlay';
+import {NgModule} from '@angular/core';
+import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
+
+@NgModule({
+    imports: [MatDialogModule, NoopAnimationsModule],
+    exports: [MatDialogModule],
+    entryComponents: [
+        AppointmentDialogComponent
+    ],
+})
+class DialogTestModule {
+}
 
 describe('AppointmentDialogComponent', () => {
-  let component: AppointmentDialogComponent;
-  let fixture: ComponentFixture<AppointmentDialogComponent>;
+    let component: AppointmentDialogComponent;
+    let fixture: ComponentFixture<AppointmentDialogComponent>;
+    let dialog: MatDialog;
+    let overlayContainerElement: HTMLElement;
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [DialogTestModule, RxReactiveFormsModule],
+            declarations: [AppointmentDialogComponent],
+            providers: [
+                {
+                    provide: OverlayContainer, useFactory: () => {
+                        overlayContainerElement = document.createElement('div');
+                        return {getContainerElement: () => overlayContainerElement};
+                    }
+                },
+                {
+                    provide: MAT_DIALOG_DATA, useValue: {}
+                }
+            ],
+        })
+        ;
+    }));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppointmentDialogComponent ]
-    })
-    .compileComponents();
-  }));
+    const initValues = () => {
+        fixture = TestBed.createComponent(AppointmentDialogComponent);
+        component = fixture.componentInstance;
+        dialog = TestBed.inject(MatDialog);
+        fixture.detectChanges();
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppointmentDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: {}});
+        TestBed.compileComponents();
+        initValues();
+        dialog.open(AppointmentDialogComponent);
+        const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
+        expect(appointmentDialog).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should render with Date to create new Appointment ', async(function() {
+        const data: any = {date: '2020-02-10'};
+        TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: data});
+        TestBed.compileComponents();
+        initValues();
+        dialog.open(AppointmentDialogComponent);
+        fixture.detectChanges();
+
+        const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
+        expect(appointmentDialog).toBeTruthy();
+        expect(component.data).toEqual(data);
+        const formValue = component.form.value;
+
+        expect(formValue.date).toEqual(data.date);
+        expect(formValue.id).toEqual('');
+        expect(formValue.timeInit).toEqual('');
+        expect(formValue.timeEnd).toEqual('');
+        expect(formValue.subject).toEqual('');
+        expect(formValue.description).toEqual('');
+        expect(formValue.area).toEqual('');
+
+        // const selectFormControlValue = (selector) => {
+        //     const element = fixture.debugElement.query(By.css(`#${selector}`));
+        //     expect(element).toBeTruthy();
+        //     return element.nativeElement.value;
+        // };
+        // fixture.detectChanges();
+        //
+        // fixture.whenStable().then(() => {
+        //     console.log('selectFormControlValue()', selectFormControlValue('date'));
+        //     console.log('textContent', overlayContainerElement.querySelector('app-appointment-dialog').textContent);
+        //
+        //     expect(selectFormControlValue('date')).toEqual(data.date);
+        //
+        // });
+    }));
+
 });
--- src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.ts
+++ src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {AppointmentFormModel} from '@app/core/model/appointment/appointment.model';
+import {FormGroup} from '@angular/forms';
+import {RxFormBuilder} from '@rxweb/reactive-form-validators';
 
 @Component({
-  selector: 'app-appointment-dialog',
-  templateUrl: './appointment-dialog.component.html',
-  styleUrls: ['./appointment-dialog.component.css']
+    selector: 'app-appointment-dialog',
+    templateUrl: './appointment-dialog.component.html',
+    styleUrls: ['./appointment-dialog.component.css']
 })
 export class AppointmentDialogComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private formB: RxFormBuilder,
+                @Optional() @Inject(MAT_DIALOG_DATA) public data: AppointmentFormModel | any) {
+    }
+
+    ngOnInit() {
+        const appointments = new AppointmentFormModel();
+        if (!!this.data) {
+            // console.log('ngOnInit', this.data);
+
+            appointments.id = this.data.id || '';
+            appointments.date = this.data.date || '';
+            appointments.timeInit = this.data.timeStart || '';
+            appointments.timeEnd = this.data.timeEnd || '';
+            appointments.subject = this.data.subject || '';
+            appointments.description = this.data.description || '';
+            appointments.area = this.data.area || '';
+
+        }
+        this.form = this.formB.formGroup(appointments);
+        // console.log('this.form.value', this.form.get('date').value);
+
+    }
+
 
 }
