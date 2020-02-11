da8bd7c092f4a7eaf5b8e59a3d5a1de1882e4e1a
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-11 18:36:57 -0500

    Fix(enviroments):Fix url to appointment api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     api: {
         menu: '/assets/menu/menu.json',
-        appointment: 'https://appointments-backend.herokuapp.com'
+        appointment: 'https://appointments-backend.herokuapp.com/api'
     }
 };
