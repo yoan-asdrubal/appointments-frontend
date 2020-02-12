63ce60cde64453552dc4d0fe7ec7163d4bb53887
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-12 11:56:54 -0500

    Refactor(package.json):Add ngx-contextmenu and ngx-take-until-destroy dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {CalendarModule, DateAdapter} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {DirectivesModule} from '@app/shared/directives/directives.module';
+import {ContextMenuModule} from 'ngx-contextmenu';
 
 @NgModule({
     declarations: [],
@@ -16,13 +23,27 @@ import {MatButtonModule} from '@angular/material/button';
             provide: DateAdapter,
             useFactory: adapterFactory
         }),
-        MatButtonModule
+        ContextMenuModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatInputModule,
+        DirectivesModule
     ],
     exports: [
         ReactiveFormsModule,
         RxReactiveFormsModule,
+        ContextMenuModule,
         CalendarModule,
-        MatButtonModule
+        MatButtonModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatInputModule,
+        DirectivesModule
     ]
 })
 export class SharedModule {
