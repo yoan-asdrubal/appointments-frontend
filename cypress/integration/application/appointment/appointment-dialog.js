43ebb1c1109d17b1855e03f7d5ad4235d1185607
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-13 23:44:49 -0500

    e2e(Appointment):Add test and implementation to show dialog from header of day of week in week mode
    Add test to check appointment default values when create it from calendar week view mode and select header of week, with default values timeInit='8:00 AM' and timeEnd='5:00 PM', all day available.

--- cypress/integration/application/appointment/appointment-dialog.js
+++ cypress/integration/application/appointment/appointment-dialog.js
@@ -67,5 +67,33 @@ describe('Appointment Dialog', () => {
             });
     });
 
-    
+    it('should show dialog with values when create from header of WEEK view mode from random day', function () {
+        cy.visit('/appointment');
+        cy.get(`[data-cy="btn-week-view"]`).click();
+        let pos = 0;
+        cy.get('.cal-header').as('query');
+        cy.get('@query').its('length')
+            .then((length) => {
+                pos = (Math.floor((Math.random() * length)));
+                cy.get('@query').its(pos).rightclick();
+
+                cy.get('.sh-context-menu--item div').its(0).click();
+                cy.get('app-appointment-dialog').should('have.length', 1);
+
+                console.log('pos', pos);
+
+                cy.get('@query').its(pos).invoke('attr', 'date')
+                    .then((val) => {
+                        const date = new Date(val);
+                        const dateStr = `${addZeroToDigit(date.getMonth() + 1)}-${addZeroToDigit(date.getDate())}-${date.getFullYear()}`;
+
+                        const timeInit = '8:00 AM';
+                        const timeEnd = '5:00 PM';
+                        cy.get('[data-cy="appointment-date"]').should('have.value', dateStr);
+                        cy.get('[data-cy="appointment-timeInit"]').should('have.value', timeInit);
+                        cy.get('[data-cy="appointment-timeInit"]').should('have.value', timeInit);
+                        cy.get('[data-cy="appointment-timeEnd"]').should('have.value', timeEnd);
+                    });
+            });
+    });
 })
