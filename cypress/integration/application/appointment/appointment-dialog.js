8ff235b7cb0b888b4270f42b7fe98e742b3a8208
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-13 23:50:51 -0500

    e2e(Appointment):Add test and implementation to show dialog from day of DAY view mode
    Add test to check appointment default values when create it from calendar day view mode at any time

--- cypress/integration/application/appointment/appointment-dialog.js
+++ cypress/integration/application/appointment/appointment-dialog.js
@@ -35,7 +35,7 @@ describe('Appointment Dialog', () => {
                     });
             });
     });
-    it('should show dialog with values when create from any time of day calendar WEEK view mode', function () {
+    it('should show dialog with values when create from any time of random day calendar WEEK view mode', function () {
         cy.visit('/appointment');
         cy.get(`[data-cy="btn-week-view"]`).click();
         let pos = 0;
@@ -67,7 +67,7 @@ describe('Appointment Dialog', () => {
             });
     });
 
-    it('should show dialog with values when create from header of WEEK view mode from random day', function () {
+    it('should show dialog with values when create from random header of WEEK view mode', function () {
         cy.visit('/appointment');
         cy.get(`[data-cy="btn-week-view"]`).click();
         let pos = 0;
@@ -88,6 +88,38 @@ describe('Appointment Dialog', () => {
                         const dateStr = `${addZeroToDigit(date.getMonth() + 1)}-${addZeroToDigit(date.getDate())}-${date.getFullYear()}`;
 
                         const timeInit = '8:00 AM';
+                        const timeEnd = '5:00 PM';
+                        cy.get('[data-cy="appointment-date"]').should('have.value', dateStr);
+                        cy.get('[data-cy="appointment-timeInit"]').should('have.value', timeInit);
+                        cy.get('[data-cy="appointment-timeInit"]').should('have.value', timeInit);
+                        cy.get('[data-cy="appointment-timeEnd"]').should('have.value', timeEnd);
+                    });
+            });
+    });
+
+    it('should show dialog with values when create from random hour of DAY view mode ', function () {
+        cy.visit('/appointment');
+        cy.get(`[data-cy="btn-day-view"]`).click();
+        let pos = 0;
+        cy.get('.cal-hour-segment:not([date*="17:30"])').as('query');
+        cy.get('@query').its('length')
+            .then((length) => {
+                pos = (Math.floor((Math.random() * length)));
+                cy.get('@query').its(pos).rightclick();
+
+                cy.get('.sh-context-menu--item div').its(0).click();
+                cy.get('app-appointment-dialog').should('have.length', 1);
+
+                console.log('pos', pos);
+
+                cy.get('@query').its(pos).invoke('attr', 'date')
+                    .then((val) => {
+                        const date = new Date(val);
+                        const dateStr = `${addZeroToDigit(date.getMonth() + 1)}-${addZeroToDigit(date.getDate())}-${date.getFullYear()}`;
+
+                        const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+                        const minutes = addZeroToDigit(date.getMinutes());
+                        const timeInit = date.toString().indexOf('00:00:00') > -1 ? '8:00 AM' : `${hour}:${minutes}${date.getHours() < 12 ? ' AM' : ' PM'}`;
                         const timeEnd = '5:00 PM';
                         cy.get('[data-cy="appointment-date"]').should('have.value', dateStr);
                         cy.get('[data-cy="appointment-timeInit"]').should('have.value', timeInit);
