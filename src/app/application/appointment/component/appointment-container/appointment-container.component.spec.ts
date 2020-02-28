221b5bf9654bc3b26bccd2f5a936c2e5d4dc4eea
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2020-02-28 15:06:07 -0500

    Fix(AppointmentContainer):Add MatDialogRef provider to test

--- src/app/application/appointment/component/appointment-container/appointment-container.component.spec.ts
+++ src/app/application/appointment/component/appointment-container/appointment-container.component.spec.ts
@@ -10,50 +10,52 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {ShContextMenuModule} from 'ng2-right-click-menu';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 describe('AppointmentContainerComponent', () => {
-    let component: AppointmentContainerComponent;
-    let fixture: ComponentFixture<AppointmentContainerComponent>;
-    let service: AppointmentService | any;
-    const data = appointmentMockData;
+  let component: AppointmentContainerComponent;
+  let fixture: ComponentFixture<AppointmentContainerComponent>;
+  let service: AppointmentService | any;
+  const data = appointmentMockData;
 
-    beforeEach(async(() => {
-        service = {
-            list: jest.fn(() => {
-                return of(data);
-            })
-        };
+  beforeEach(async(() => {
+    service = {
+      list: jest.fn(() => {
+        return of(data);
+      })
+    };
 
-        TestBed.configureTestingModule({
-            imports: [NoopAnimationsModule, CalendarModule.forRoot({
-                provide: DateAdapter,
-                useFactory: adapterFactory
-            }), MatButtonModule, ShContextMenuModule, MatDialogModule],
-            declarations: [AppointmentContainerComponent]
-            , providers: [
-                {provide: AppointmentService, useValue: service}
-            ]
-        })
-            .compileComponents();
-    }));
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory
+      }), MatButtonModule, ShContextMenuModule, MatDialogModule, MatMenuModule, MatIconModule],
+      declarations: [AppointmentContainerComponent]
+      , providers: [
+        {provide: AppointmentService, useValue: service}
+      ]
+    })
+      .compileComponents();
+  }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(AppointmentContainerComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppointmentContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get list of appointments', function() {
+    const appointments$ = component.appointments$;
+    appointments$.pipe().subscribe((items: any[]) => {
+      expect(items.length).toEqual(3);
+      expect(items).toEqual(data);
     });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
-
-    it('should get list of appointments', function() {
-        const appointments$ = component.appointments$;
-        appointments$.pipe().subscribe((items: any[]) => {
-            expect(items.length).toEqual(3);
-            expect(items).toEqual(data);
-        });
-        expect(service.list).toHaveBeenCalled();
-        expect(service.list).toHaveBeenCalledTimes(1);
-    });
+    expect(service.list).toHaveBeenCalled();
+    expect(service.list).toHaveBeenCalledTimes(1);
+  });
 });
--- src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.spec.ts
+++ src/app/application/appointment/component/appointment-dialog/appointment-dialog.component.spec.ts
@@ -1,7 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AppointmentDialogComponent} from './appointment-dialog.component';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {OverlayContainer} from '@angular/cdk/overlay';
 import {NgModule} from '@angular/core';
@@ -12,110 +12,80 @@ import {MatInputModule} from '@angular/material/input';
 import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 
 @NgModule({
-    imports: [MatDialogModule, NoopAnimationsModule],
-    exports: [MatDialogModule],
-    entryComponents: [
-        AppointmentDialogComponent
-    ],
+  imports: [MatDialogModule, NoopAnimationsModule],
+  exports: [MatDialogModule],
+  entryComponents: [
+    AppointmentDialogComponent
+  ],
 })
 class DialogTestModule {
 }
 
 describe('AppointmentDialogComponent', () => {
-    let component: AppointmentDialogComponent;
-    let fixture: ComponentFixture<AppointmentDialogComponent>;
-    let dialog: MatDialog;
-    let overlayContainerElement: HTMLElement;
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [DialogTestModule,
-                RxReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule,
-                NgxMaterialTimepickerModule],
-            declarations: [AppointmentDialogComponent],
-            providers: [
-                {
-                    provide: OverlayContainer, useFactory: () => {
-                        overlayContainerElement = document.createElement('div');
-                        return {getContainerElement: () => overlayContainerElement};
-                    }
-                },
-                {
-                    provide: MAT_DIALOG_DATA, useValue: {}
-                }
-            ],
-        })
-        ;
-    }));
+  let component: AppointmentDialogComponent;
+  let fixture: ComponentFixture<AppointmentDialogComponent>;
+  let dialog: MatDialog;
+  let overlayContainerElement: HTMLElement;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [DialogTestModule,
+        RxReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule,
+        NgxMaterialTimepickerModule],
+      declarations: [AppointmentDialogComponent],
+      providers: [
+        {
+          provide: OverlayContainer, useFactory: () => {
+            overlayContainerElement = document.createElement('div');
+            return {getContainerElement: () => overlayContainerElement};
+          }
+        },
+        {
+          provide: MAT_DIALOG_DATA, useValue: {}
+        },
+        {provide: MatDialogRef, useValue: {}}
+      ],
+    })
+    ;
+  }));
 
-    const initValues = () => {
-        fixture = TestBed.createComponent(AppointmentDialogComponent);
-        component = fixture.componentInstance;
-        dialog = TestBed.inject(MatDialog);
-        fixture.detectChanges();
-    };
+  const initValues = () => {
+    fixture = TestBed.createComponent(AppointmentDialogComponent);
+    component = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog);
+    fixture.detectChanges();
+  };
 
-    it('should create', () => {
-        TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: {}});
-        TestBed.compileComponents();
-        initValues();
-        dialog.open(AppointmentDialogComponent);
-        const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
-        expect(appointmentDialog).toBeTruthy();
-    });
+  it('should create', () => {
+    TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: {}});
+    TestBed.compileComponents();
+    initValues();
+    dialog.open(AppointmentDialogComponent);
+    const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
+    expect(appointmentDialog).toBeTruthy();
+  });
 
-    it('should render with Date to create new Appointment ', async(function() {
-        const data: any = {date: new Date(2020, 1, 10, 8, 0)};
-        TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: data});
-        TestBed.compileComponents();
-        initValues();
-        dialog.open(AppointmentDialogComponent);
-        fixture.detectChanges();
+  it('should render with Date to create new Appointment ', async(function() {
+    const data: any = {date: new Date(2020, 1, 10, 8, 0)};
+    TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: data});
+    TestBed.compileComponents();
+    initValues();
+    dialog.open(AppointmentDialogComponent);
+    fixture.detectChanges();
 
-        const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
-        expect(appointmentDialog).toBeTruthy();
-        expect(component.data).toEqual(data);
-        const formValue = component.form.value;
+    const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
+    expect(appointmentDialog).toBeTruthy();
+    expect(component.data).toEqual(data);
+    const formValue = component.form.value;
 
-        expect(formValue.date).toEqual('02-10-2020');
-        expect(formValue.id).toEqual('');
-        expect(formValue.timeInit).toEqual('08:00 AM');
-        expect(formValue.timeEnd).toEqual('05:00 PM');
-        expect(formValue.subject).toEqual('');
-        expect(formValue.description).toEqual('');
-        expect(formValue.area).toEqual('');
+    expect(formValue.date).toEqual('02-10-2020');
+    expect(formValue.id).toEqual('');
+    expect(formValue.timeInit).toEqual('08:00 AM');
+    expect(formValue.timeEnd).toEqual('05:00 PM');
+    expect(formValue.subject).toEqual('');
+    expect(formValue.description).toEqual('');
+    expect(formValue.area).toEqual('');
 
-    }));
+  }));
 
-    it('should init formGroup with default values to edit appointment', async(function() {
-        const data: any = {
-            id: 'id1',
-            date: '2020-02-10',
-            timeInit: '10:00 AM',
-            timeEnd: '12:00 PM',
-            subject: 'Todo Time',
-            description: 'Time to make some todos',
-            area: 'TodoArea'
-        };
-        TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: data});
-        TestBed.compileComponents();
-        initValues();
-        dialog.open(AppointmentDialogComponent);
-        fixture.detectChanges();
-
-        const appointmentDialog = overlayContainerElement.querySelector('app-appointment-dialog');
-        expect(appointmentDialog).toBeTruthy();
-        expect(component.data).toEqual(data);
-        const formValue = component.form.value;
-
-        expect(formValue.date).toEqual(data.date);
-        expect(formValue.id).toEqual(data.id);
-        expect(formValue.timeInit).toEqual(data.timeInit);
-        expect(formValue.timeEnd).toEqual(data.timeEnd);
-        expect(formValue.subject).toEqual(data.subject);
-        expect(formValue.description).toEqual(data.description);
-        expect(formValue.area).toEqual(data.area);
-
-
-    }));
 
 });
